42d2fe1adacd58005f20c549e0957a2c060ac9db
Adrian Sims <asims@parkeon.com>
2017-11-03 12:23:35 +0000

    First Commit

---
+++ Prog4/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Prog4
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            try
+            {
+            }
+            catch (Exception ex)
+            {
+                var codeBase = System.Reflection.Assembly.GetExecutingAssembly().CodeBase;
+                var progname = Path.GetFileNameWithoutExtension(codeBase);
+                Console.Error.WriteLine(progname + ": Error: " + ex.Message);
+            }
+
+        }
+    }
+}
